970363e5c354792fe5c5295aff1c54244be27130
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-08-27 21:37:31 +0400

    01/13 annotations @Configuration, @ComponentScan, @PropertySource

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -1,44 +1,14 @@
 package ru.alishev.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
-//        Music classicalMusic = context.getBean("musicBean1", ClassicalMusic.class);
-//        Music rockMusic = context.getBean("someRockMusic", RockMusic.class);
-//        Music rapMusic = context.getBean("rapMusic", RapMusic.class);
-//        System.out.println(classicalMusic.getSong());
-//        System.out.println(rockMusic.getSong());
-//        System.out.println(rapMusic.getSong());
-//
-//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-//
-//        System.out.println(comparison);
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(10);
-//
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
-
-//        firstMusicPlayer.playMusicList();
-//
-//        System.out.println(firstMusicPlayer.getName());
-//        System.out.println(firstMusicPlayer.getVolume());
-
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
-
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
